80b85b891f001b6f3b4e5e47b2601eb2e961cc42
Marcelo Cortes <marcelo.cortes@instivo.com.br>
2023-03-20 10:56:31 -0300

    heading and text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam nobis deserunt doloribus! Atque fuga veniam odio ad illum voluptatibus nam architecto! Nemo unde, inventore quaerat veritatis provident voluptatum cumque tenetur.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
