7940ff0f932788acbe67bf9e2241ac58e335d181
Marcelo Cortes <marcelo.cortes@instivo.com.br>
2023-03-25 22:35:24 -0300

    melhorias storybook

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/marcelotc.png',
     alt: 'Marcelo Cortes',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,15 +1,17 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Box, Text, BoxProps } from '@ignite-ui/react'
-
+import { Box, BoxProps, Text } from '@ignite-ui/react'
 export default {
   title: 'Surfaces/Box',
   component: Box,
   args: {
-    children: (
-      <>
-        <Text>Testando o elemento Box</Text>
-      </>
-    ),
+    children: <Text>Testando o elemento Box</Text>,
+  },
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
   },
 } as Meta<BoxProps>
 
